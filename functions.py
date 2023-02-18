9e31a7d7462da13d4928b408f16d4b4aea9d985d
biancasutu <bianca.andreea.sutu@gmail.com>
2023-02-18 14:22:28 +0200

    First commit for deploying the app in cloud

---
+++ functions.py
@@ -0,0 +1,25 @@
+FILEPATH = 'to_do_app/todos.txt'
+
+
+def get_todos(filepath=FILEPATH):
+    """ Read a text file and return the list
+    of to-do items
+    This text is called --doc (documentation) strings--
+    It prints the text(documentation) when you write
+    help(function) in console
+    """
+    with open(filepath, 'r') as f:
+        todos_local  = f.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    with open(filepath, 'w') as f:
+        f.writelines(todos_arg)
+
+
+print(__name__)
+
+if __name__ == '__main__':
+    print('Hello from functions module')
+    print(get_todos())
---
+++ web_app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import functions
+
+# streamlit run to_do_app/web.py
+# streamlit = framework for data analytics and machine learning
+
+todos = functions.get_todos()
+
+
+def add_todo():     # this is a callback function
+    todo = '\n' + st.session_state['new_todo']
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
+st.title('My Todo App')
+
+
+for todo in todos:
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.remove(todo)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()  # update in real time withot refreshing the webpage
+
+st.text_input(label='', placeholder='Add a new todo...',
+                on_change=add_todo, key='new_todo')
